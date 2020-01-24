be31bc34c293258eadf4babcf539877f6d69fea1
Aadit Kamat <aadit.k12@gmail.com>
2020-01-23 23:46:58 -0500

    Add A1 images and test files

--- A1/src/TokenCounter.java
+++ A1/src/TokenCounter.java
@@ -1,23 +1,32 @@
+import gradingTools.comp533s19.assignment0.AMapReduceTracer;
 import util.trace.Tracer;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-public class TokenCounter {
+public class TokenCounter extends AMapReduceTracer{
     public static void main(String[] args) {
+        TokenCounter counter = new TokenCounter();
         Scanner inputHandler = new Scanner(System.in);
-        String[] tokens = inputHandler.nextLine().split(" ");
-        Map<String, Integer> result = new HashMap<String, Integer>();
-        for (String token: tokens) {
-            if (result.get(token) == null) {
-                result.put(token, 1);
-            } else {
-                int value = result.get(token);
-                result.put(token, value + 1);
+        while (true)  {
+            counter.traceNumbersPrompt();
+            String line = inputHandler.nextLine();
+            if (line.equals(AMapReduceTracer.QUIT)) {
+                return;
             }
+            String[] tokens = line.split(" ");
+            Map<String, Integer> result = new HashMap<String, Integer>();
+            for (String token: tokens) {
+                if (result.get(token) == null) {
+                    result.put(token, 1);
+                } else {
+                    int value = result.get(token);
+                    result.put(token, value + 1);
+                }
+            }
+            String resultOutput = result.toString();
+            System.out.println(resultOutput.substring(1, resultOutput.length() - 1));
         }
-        String resultOutput = result.toString();
-        Tracer.userMessage(resultOutput.substring(1, resultOutput.length() - 1));
     }
 }
