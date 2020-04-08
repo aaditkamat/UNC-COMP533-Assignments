445fee8f49f7b890e74a496892ccbbf1c0ba45dc
Aadit Kamat <aadit.k12@gmail.com>
2020-04-08 12:57:23 +0800

    Remove original template code

--- A4/src/coupledsims/model/RemoteStandAloneTwoCoupledHalloweenSimulations.java
+++
@@ -1,9 +0,0 @@
-package coupledsims.model;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface RemoteStandAloneTwoCoupledHalloweenSimulations extends Remote {
-	void start(String[] args) throws RemoteException;
-	void trace(boolean newValue) throws RemoteException;
-}
